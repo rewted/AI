91dd23b2e45b8f14bfc9d125cce78165c654e59f
rewted <rewtguy@gmail.com>
2017-10-10 06:50:31 -0700

    Working on sorting Population

---
+++ project4/tsp4.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+
+using namespace std;
+
+typedef struct Point {
+  int id;
+  double x, y;
+} Point;
+
+typedef struct Chromo {
+  int id;
+  list<int> tour;
+  double fitness;
+} Chromo;
+
+typedef struct Population {
+  int generation;
+  double averageFitness, bestFitness, worstFitness;
+  int averageId, bestId, worstId;
+  vector<Chromo> citizens;
+} Population;
+
+// Global Variables
+Point points[100];
+int totalCount = 100;
+
+void insert (Point *point, int i, double x, double y) {
+  point->id = i;
+  point->x = x;
+  point->y = y;
+}
+
+int readFile (char * fileName, Point *points) {
+  string buffer;
+  string tmp;
+  int count = 0;
+  int id;
+  double x, y;
+  ifstream tspFile (fileName);
+  if (!tspFile.is_open()) {
+    cout << "File failed to load" << endl;
+  }
+
+  for (int i = 0; i < 7; i++) {
+    getline (tspFile, buffer);
+  }
+
+  while (getline(tspFile, buffer)) {
+    istringstream iss (buffer);
+    iss >> tmp;
+    id = stoi (tmp);
+    iss >> tmp;
+    x = stod (tmp);
+    iss >> tmp;
+    y = stod (tmp);
+    iss >> tmp;
+
+    if (count == id -1) {
+      insert ((points+count), id, x, y);
+    }
+    count++;
+  }
+  tspFile.close();
+  return count;
+}
+
+double distanceCalc (Point firstPoint, Point secondPoint) {
+  return sqrt(pow((secondPoint.x - firstPoint.x),2) + pow((secondPoint.y - firstPoint.y),2));
+}
+
+double routeDistance (list<int> route, Point *points, int numPoints) {
+  double distance = 0;
+  list<int>::iterator it = route.begin();
+  list<int>::iterator it2 = route.begin();
+  advance(it2, numPoints-1);
+  distance = distanceCalc (points[*it-1], points[*it2-1]);
+
+  it2 = route.begin();
+  it2++;
+  while (it2 != route.end()) {
+    distance = distance + distanceCalc (points[*it-1], points[*it2-1]);
+    it++;
+    it2++;
+  }
+  return distance;
+}
+
+int random (int i) { return std::rand()%i; }
+
+void generatePopulation (Population *a) {
+  vector<int> b;
+  list<int> path;
+  Chromo tmpChromo;
+  for (int i = 0; i < 100; i++) {
+    b.push_back(i);
+  }
+  for (int i = 0; i < 100; i++) {
+    random_shuffle (b.begin(), b.end());
+    for (vector<int>::iterator it = b.begin(); it != b.end(); it++) {
+      path.push_back(*it);
+      //      cout << *it << " ";
+    }
+    //cout << endl << "-------------------" << endl;
+    tmpChromo.tour = path;
+    path.clear();
+    tmpChromo.id = i;
+    tmpChromo.fitness = routeDistance (tmpChromo.tour, points, totalCount);
+    a->citizens.push_back(tmpChromo);
+  }
+}
+
+void sortPopulation (Population *a) {
+  vector<Chromo> b;
+  vector<Chromo> c = a->citizens;
+  double lowest = 999999999999;
+  for (int i = 0; i < 100; i++) {
+    
+  }
+  
+}
+
+int main (int argc, char *argv[]) {
+  if (argc < 2) {
+    cout << "Include file name when executing." << endl;
+    return 0;
+  }
+  totalCount = readFile (argv[1], points);
+  Population world;
+  generatePopulation(&world);
+  
+  for (int i = 0; i < 100; i++) {
+    cout << "-----------id: " << i << "------------fitness: " << world.citizens[i].fitness <<  "-----------" << endl;
+    for (list<int>::iterator it = world.citizens[i].tour.begin(); it != world.citizens[i].tour.end(); it++) {
+      cout << *it << " ";
+    }
+    cout << endl << endl;
+  }
+  
+
+  /*
+  srand (unsigned (time (0)));
+  vector<vector<int>> b (100, vector<int>(0));;
+  list<int> a;
+  list<int>::iterator it = a.begin();
+  for (int i = 0; i < 15; i++) {
+    for (int j = 0; j < 100; j++) {
+      b[j].push_back(i);
+    }
+    a.push_back(i);
+    //    cout << *it << endl;
+  }
+  for (int i = 0; i < 100; i++) {
+    random_shuffle (b[i].begin(), b[i].end());
+    for (vector<int>::iterator it2 = b[i].begin(); it2 != b[i].end(); it2++) {
+      cout << *it2 << " ";
+    }
+    cout << endl;
+  }
+  */
+  return 0;
+}
