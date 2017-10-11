b6034ee6bc9090ca293c40336519661acc98f00d
rewted <rewtguy@gmail.com>
2017-10-11 02:36:17 -0700

    Almost finished with PMX Crossover

--- project4/tsp4.cpp
+++ project4/tsp4.cpp
@@ -161,12 +161,9 @@ void generationStats (Population *a) {
 void generationMutate (Population *a, int mutationRate) {
   int nodeA = 0;
   int nodeB = 0;
-  int tmpInt;
   list<int>::iterator it;
   list<int>::iterator it2;
-  //list<int>::iterator it3;
   for (int i = 0; i < 100; i++) {
-    //cout << mutationRate << endl;
     if (random (mutationRate) == 0) {
       while (nodeA == nodeB) {
 	nodeA = random (100);
@@ -176,33 +173,12 @@ void generationMutate (Population *a, int mutationRate) {
       it2 = a->citizens[i].tour.begin();
       advance (it, nodeA);
       advance (it2, nodeB);
-      //cout << *it << " " << *it2;
-      /*
-      cout << endl;
-      it3 = a->citizens[i].tour.begin();
-      cout << "prior to swap" << endl;
-      while (it3 != a->citizens[i].tour.end()) {
-	cout << *it3 << " ";
-	it3++;
-      }
-      cout << endl;
-      */      
       a->citizens[i].tour.insert(it, *it2);
       a->citizens[i].tour.insert(it2, *it);
       it = a->citizens[i].tour.erase(it);
       it2 = a->citizens[i].tour.erase(it2);
       a->citizens[i].fitness = routeDistance (a->citizens[i].tour, points, totalCount);
-      /*
-      it3 = a->citizens[i].tour.begin();
-      cout << endl;
-      cout << "post swap" << endl;
-      while (it3 != a->citizens[i].tour.end()) {
-	cout << *it3 << " ";
-	it3++;
-      }
-      cout << endl;
-      */
-      cout << "Mutate: " << i << "\tSwapping " << nodeA << " and " << nodeB << endl;
+      //cout << "Mutate: " << i << "\tSwapping " << nodeA << " and " << nodeB << endl;
     }
     nodeA = 0;
     nodeB = 0;
@@ -220,6 +196,202 @@ void printWorld (Population *a) {
   }
 }
 
+void pmxCrossover (Population *a) {
+  // kill 30 least fit in the population
+  for (int i = 0; i < 40; i++) {
+    a->citizens.pop_back();
+    //cout << "Pop citizen" << endl;
+  }
+  int count = 0;
+  int parentA = 0;
+  int parentB = 0;
+  int crossoverSite;
+  list<int> childA;
+  list<int> childB;
+  vector<int> aCross;
+  vector<int> bCross;
+  list<int>::iterator itParentA1;
+  list<int>::iterator itParentA2;
+  list<int>::iterator itParentB1;
+  list<int>::iterator itParentB2;
+  list<int>::iterator itChild1;
+  list<int>::iterator itChild2;
+  list<int>::iterator ptr;
+  double percentA = 0;
+  double percentB = 0;
+  double chance = 0;
+  // need to replace 30 children
+  // two created at a time
+  while (count != 30) {
+    // select two different parents
+    while (parentA == parentB) {
+      parentA = random (60);
+      parentB = random (60);
+    }
+    // percent gradent dependent on fitness
+    // for chance to mate between parentA and parent B
+    if (parentA < 15) {
+      percentA = 0.80;
+    }
+    if (parentA >= 15 && parentA < 30) {
+      percentA = 0.70;
+    }
+    if (parentA >= 30 && parentA < 45) {
+      percentA = 0.60;
+    }
+    if (parentA >= 45 && parentA < 60) {
+      percentA = 0.50;
+    }
+    if (parentB < 15) {
+      percentA = 0.80;
+    }
+    if (parentB >= 15 && parentB < 30) {
+      percentB = 0.70;
+    }
+    if (parentB >= 30 && parentB < 45) {
+      percentB = 0.60;
+    }
+    if (parentB >= 45 && parentB < 60) {
+      percentB = 0.50;
+    }
+    chance = percentA * percentB;
+    //cout << "Parent A: " << parentA << " Parent B: " << parentB << " Chance: " << chance << endl;
+    // mating successful initiate PMX Crossover
+    if (random((1/chance)) == 0) {
+      crossoverSite = random (96);
+      cout << "----------------" << endl;
+      cout << "Do cross over: " << count << " at site: " << crossoverSite << endl;
+      itParentA1 = a->citizens[parentA].tour.begin();
+      itParentA2 = a->citizens[parentA].tour.begin();
+      itParentB1 = a->citizens[parentB].tour.begin();
+      itParentB2 = a->citizens[parentB].tour.begin();
+      advance (itParentA1, crossoverSite);
+      advance (itParentA2, crossoverSite + 5);
+      advance (itParentB1, crossoverSite);
+      advance (itParentB2, crossoverSite + 5);
+      // getting values for deduping
+      ptr = itParentA1;
+      //cout << "[ ";
+      while (ptr != itParentA2) {
+	//cout << *ptr << " ";
+	aCross.push_back(*ptr);
+	ptr++;
+      }
+      // getting values for deduping
+      ptr = itParentB1;
+      //cout <<"]----[ ";
+      while (ptr != itParentB2) {
+	bCross.push_back(*ptr);
+	//cout << *ptr << " ";
+	ptr++;
+      }
+      // deduping crossing values
+      cout << "Deduping" <<endl;
+      vector<int>::iterator itCrossA = aCross.begin();
+      vector<int>::iterator itCrossB = bCross.begin();
+      while (itCrossA != aCross.end()) {
+	itCrossB = bCross.begin();
+	while (itCrossB != bCross.end()) {
+	  if (*itCrossA == *itCrossB) {
+	    cout << "Erasing: " << *itCrossA << endl;
+	    itCrossA = aCross.erase(itCrossA);
+	    itCrossB = bCross.erase(itCrossB);
+	    if (itCrossB == bCross.end()) {
+	      cout << "B: Deleting an end" << endl;
+	      break;
+	    }
+	  }
+	  itCrossB++;
+	}
+	if (itCrossA == aCross.end()) {
+	  cout << "A: deleting an end" << endl;
+	  break;
+	}
+	itCrossA++;
+      }
+      itCrossA = aCross.begin();
+      itCrossB = bCross.begin();
+      cout << "[ ";
+      while (itCrossA != aCross.end()) {
+	cout << *itCrossA << " ";
+	itCrossA++;
+      }
+      cout << "] [ ";
+      while (itCrossB != bCross.end()) {
+	cout << *itCrossB << " ";
+	itCrossB++;
+      }
+      cout << "]" << endl;
+      //cout << "]" << endl;
+      // Starting to create childA
+      ptr = a->citizens[parentA].tour.begin();
+      while (ptr != a->citizens[parentA].tour.end()) {
+	childA.push_back(*ptr);
+	ptr++;
+	if (ptr == itParentA1) {
+	  ptr = itParentB1;
+	}
+	if (ptr == itParentB2) {
+	  ptr = itParentA2;
+	}
+      }
+      // Starting to create childB
+      ptr = a->citizens[parentB].tour.begin();
+      while (ptr != a->citizens[parentB].tour.end()) {
+	childB.push_back(*ptr);
+	ptr++;
+	if (ptr == itParentB1) {
+	  ptr = itParentA1;
+	}
+	if (ptr == itParentA2) {
+	  ptr = itParentB2;
+	}
+      }
+      // removes duplicate nodes in the child
+      ptr = childA.begin();
+      itChild1 = childA.begin();
+      itChild2 = childA.begin();
+      advance (itChild1, crossoverSite);
+      advance (itChild2, crossoverSite + 5);
+      itCrossA = aCross.begin();
+      itCrossB = bCross.begin();
+      while (ptr != childA.end()) {
+	if (ptr == itChild1) {
+	  ptr = itChild2;
+	}
+	itCrossB = bCross.begin();
+	while (itCrossB != bCross.end()) {
+	  if (*ptr == *itCrossB) {
+	    cout << "Equal " << *ptr << endl;
+	  }
+	  itCrossB++;
+	}
+	ptr++;
+      }
+      
+      /*
+      for (list<int>::iterator a = childA.begin(); a != childA.end(); a++) {
+	cout << *a << " ";
+      }
+      cout << endl << endl;
+      for (list<int>::iterator a = childB.begin(); a != childB.end(); a++) {
+	cout << *a << " ";
+      }
+      */
+      cout << endl << endl;
+      count++;
+    }
+    aCross.clear();
+    bCross.clear();
+    childA.clear();
+    childB.clear();
+    chance = 0;
+    parentA = 0;
+    parentB = 0;
+  }
+
+}
+
 int main (int argc, char *argv[]) {
   if (argc < 2) {
     cout << "Include file name when executing." << endl;
@@ -233,28 +405,7 @@ int main (int argc, char *argv[]) {
   generationMutate (&world, mutationRate);
   generationSort (&world);
   generationStats (&world);
-  printWorld (&world);
-  
-
-  /*
-  srand (unsigned (time (0)));
-  vector<vector<int>> b (100, vector<int>(0));;
-  list<int> a;
-  list<int>::iterator it = a.begin();
-  for (int i = 0; i < 15; i++) {
-    for (int j = 0; j < 100; j++) {
-      b[j].push_back(i);
-    }
-    a.push_back(i);
-    //    cout << *it << endl;
-  }
-  for (int i = 0; i < 100; i++) {
-    random_shuffle (b[i].begin(), b[i].end());
-    for (vector<int>::iterator it2 = b[i].begin(); it2 != b[i].end(); it2++) {
-      cout << *it2 << " ";
-    }
-    cout << endl;
-  }
-  */
+  pmxCrossover (&world);
+  //printWorld (&world);
   return 0;
 }
